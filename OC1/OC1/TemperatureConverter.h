0524eaddd568180c97eadbeaeb361216d0f61599
Lucas <lucas@agtlucas.com>
2015-04-03 09:27:03 -0300

    Adding TemperatureConverter class

---
+++ OC1/OC1/TemperatureConverter.h
@@ -0,0 +1,13 @@
+//
+//  TemperatureConverter.h
+//  OC1
+//
+//  Created by Lucas on 4/3/15.
+//  Copyright (c) 2015 AWLKA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TemperatureConverter : NSObject
+
+@end
---
+++ OC1/OC1/TemperatureConverter.m
@@ -0,0 +1,13 @@
+//
+//  TemperatureConverter.m
+//  OC1
+//
+//  Created by Lucas on 4/3/15.
+//  Copyright (c) 2015 AWLKA. All rights reserved.
+//
+
+#import "TemperatureConverter.h"
+
+@implementation TemperatureConverter
+
+@end
