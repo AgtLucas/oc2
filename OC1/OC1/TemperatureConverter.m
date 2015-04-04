dc57295616b87c79eea111a8f30c31e864bd2c92
Lucas <lucas@agtlucas.com>
2015-04-03 22:48:28 -0300

    Updates

--- OC1/OC1/TemperatureConverter.m
+++ OC1/OC1/TemperatureConverter.m
@@ -8,10 +8,27 @@
 
 #import "TemperatureConverter.h"
 
-@implementation TemperatureConverter
+@implementation TemperatureConverter {
+    float _totalDegreesFarenheit;
+    float _numMeasurements;
+}
+
+- (instancetype)initWithLocation:(NSString *)location {
+    if ((self = [super init])) {
+        _location = location;
+        _totalDegreesFarenheit = 0;
+        _numMeasurements = 0;
+    }
+    return self;
+}
 
 // Implement methods
 - (float)degreesFarenheitToCelsius:(float)degreesFarenheit {
+
+    _totalDegreesFarenheit += degreesFarenheit;
+    _numMeasurements++;
+    _averageDegreesFarenheit = _totalDegreesFarenheit / _numMeasurements;
+
     return 5.0/9.0 * (degreesFarenheit - 32.0);
 }
 
